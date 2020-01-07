afcf147f2e5207c897d2f3dcca85901c2fe918ac
matheuscamarques <37029621+matheuscamarques@users.noreply.github.com>
2020-01-07 17:55:45 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -8,16 +8,22 @@ int main(){
     administrador = cadastraUsuario();
 
 
-    printf("\n");
-    printf("CPF: %li \n",administrador.dados.cpf);
-    printf("NOME:%s \n ",administrador.dados.nome);
+    printf("\n ");
+    printf("CPF: %li \n ",administrador.dados.cpf);
+    printf("NUMERO:%s \n ",administrador.dados.nome);
+    printf("NOME:%li \n ",administrador.dados.telefone);
     printf("DATA NASCIMENTO:%li \n ",administrador.dados.data_nascimento);
     printf("EMAIL:%s \n ",administrador.dados.email);
+
     printf("CEP:%li \n ",administrador.dados.logradouro.cep);
     printf("RUA: %s \n ",administrador.dados.logradouro.rua);
     printf("NUMERO: %li \n ",administrador.dados.logradouro.numero);
+    
     printf("LOGIN: %s \n ",administrador.login);
     printf("SENHA: %s \n ",administrador.senha);
+
+     printf("\n"); 
+ 
 
 }
 
